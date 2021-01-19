2b3767ff0b2951774895e12a01c5ca89bea21703
Wj <Wj@DESKTOP-L7QG7V2>
2021-01-19 13:42:22 +0900

    왜 안되는거야

--- src/TestClass/TestClass2.java
+++ src/TestClass/TestClass2.java
@@ -1,10 +1,10 @@
 package TestClass;
 
-public class TestClass {
+public class TestClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello this is github practicing!");
+		System.out.println("i like music");
 		// Test Test Test
 		
 
